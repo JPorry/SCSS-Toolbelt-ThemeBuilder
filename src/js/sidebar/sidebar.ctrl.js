bc80152c391019220c6abfe1616110a4bbab2cfd
JPorrero <jporry@gmail.com>
2015-08-14 19:59:50 +0200

    Adding the ability to open/close the sidebar

--- src/js/sidebar/sidebar.ctrl.js
+++ src/js/sidebar/sidebar.ctrl.js
@@ -1,10 +1,14 @@
 'use strict';
 angular.module('baseProject')
-    .controller('sidebarCtrl', function(Variables){
+    .controller('sidebarCtrl', function(Variables, SideBar){
         var sidebar = this;
         sidebar.styles = Variables.variables;
 
         sidebar.reset = Variables.reset;
 
         sidebar.generateThemeFile = Variables.generateThemeFile;
+
+        sidebar.sidebar = SideBar.status;
+
+        sidebar.toggle = SideBar.toggle;
     });
